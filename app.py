9a2f3877ce1e3086f2b68933f00d416d0bcabc51
Vishnu Tadimeti <vtadimet@purdue.edu>
2017-01-21 04:19:23 -0500

    Add Clarifai, predict sample image

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 from flask import Flask, send_file, render_template, request
 from werkzeug.utils import secure_filename
+from clarifai.rest import ClarifaiApp
+
+
+app = ClarifaiApp("wLADJynVIqVNlpuUhS45MHkBoQ4C4RYrHqlOrLhI", "L23m2pfhwOjamIX0GCfOimH3JHDQcCx23HMnk4eR")
+
+# get the general model
+model = app.models.get("general-v1.3")
+
+# predict with the model
+
+
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
+    print model.predict_by_url(url='https://samples.clarifai.com/metro-north.jpg')
     return send_file("views/index.html")
 
 
