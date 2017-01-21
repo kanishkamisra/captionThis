76da975662c449e1b98ec1f7b94129594ec07784
Vishnu Tadimeti <vtadimet@purdue.edu>
2017-01-21 03:42:20 -0500

    File upload works

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, render_template, request
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     return send_file("views/index.html")
+
+
+@app.route('/uploader', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(secure_filename(f.filename))
+        return 'file uploaded successfully'
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
