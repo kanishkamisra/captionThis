82ee3ff02a55e653d3cff059b705017836c7cc5d
Vishnu Tadimeti <vtadimet@purdue.edu>
2017-01-22 09:20:05 -0500

    Final changes

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+from twython import Twython
 
 from flask import Flask, render_template, request
 from flask import send_from_directory
@@ -15,9 +16,16 @@ model = app.models.get("general-v1.3")
 app = Flask(__name__)
 
 # Set directory to store images
-# UPLOAD_FOLDER = '/Users/vishnutadimeti/Developer/Python/captionThis/static'
-UPLOAD_FOLDER = 'C:/Users/Kanishka/captionThis/static'
+UPLOAD_FOLDER = '/Users/vishnutadimeti/Developer/Python/captionThis/static'
+# UPLOAD_FOLDER = 'C:/Users/Kanishka/captionThis/static'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+twitter_photo = "name"
+
+twitter = Twython("mcBrpMgySLjb866aTA4Sr3EbC",
+                  "BB0o45dYYKSIQOyK5UyiMVUOKukXdKoN6ko2htRuROam44pbTX",
+                  "215693554-gZg3b3I5NBIRfHkcq8O9om75RlIrP5ZAn3O2DuE9",
+                  "mDxlFjvno3A1aKsYfOyA8sG0cTehYEMMSuq2MFnsbXeGX")
 
 
 @app.route("/")
@@ -46,17 +54,29 @@ def upload_file():
         # print model.predict([test_image])
         # filename = 'lake.jpg'
         # print(captionize(UPLOAD_FOLDER + '/' + filename))
-        captions = []
-        for caption in captionize(UPLOAD_FOLDER + '/' + filename):
-            captions.append(caption)
+
+        captions = [
+                    'You can make your card-action always visible']
 
         # Return the template display.html to render, and the filename of the image saved
         return render_template('display.html', image_name=filename, captionlist = captions)
 
 
-@app.route('/display/<filename>')
+# @app.route('/photo', methods=['GET', 'POST'])
+# def post_twitter():
+#     print "Name of the photo: " + twitter_photo
+#     photo = open(UPLOAD_FOLDER + "/" + twitter_photo, 'rb')
+#     response = twitter.upload_media(media=photo)
+#     twitter.update_status(status='Checkout this cool image!', media_ids=[response['media_id']])
+#     return twitter_photo
+
+
+@app.route('/display/<filename>', methods=['GET', 'POST'])
 def send_file(filename):
-    return send_from_directory(UPLOAD_FOLDER, filename)
+    photo = open(UPLOAD_FOLDER + "/" + filename, 'rb')
+    response = twitter.upload_media(media=photo)
+    twitter.update_status(status='Checkout this cool image!', media_ids=[response['media_id']])
+    return render_template('home.html')
 
 
 if __name__ == "__main__":
