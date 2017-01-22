b915f74de360a823f698ce59de978551c3204f89
vincentvangoat <menogetusername@gmail.com>
2017-01-22 02:16:22 -0500

    captionize baby

---
+++ captionize.py
@@ -0,0 +1,92 @@
+import requests
+import pandas as pd
+from clarifai import rest
+from clarifai.rest import ClarifaiApp
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.metrics.pairwise import linear_kernel
+
+def clarifaize(img):
+	# returns tags sorted by probability as list
+
+    app = ClarifaiApp("mrKZLVJBD6mWwmzTg_84YsbDlZRj1yQPIlBlfQZd", "vuuKXEtODxS8RHNo-a0wfF4vMJr4pVojR-mEScL3")
+    # get the general model
+    model = app.models.get("general-v1.3")
+    # predict with the model
+    # prediction = model.predict_by_url(url=img)['outputs'][0]['data']['concepts']
+    prediction = model.predict_by_filename(img)['outputs'][0]['data']['concepts']
+    tags = pd.DataFrame({'Tag': [], 'Probability': []})
+    for item in prediction:
+        tags = tags.append(pd.DataFrame({'Tag': [item['name']], 'Probability': [item['value']]}))
+#     return tags.reset_index(drop=True)
+    tags = list(tagDataset['Tag'])
+    if('no person' in tags):
+        tags.remove('no person')
+    
+    return tags
+
+def tagPhrase(tags):
+	# Returns the phrase with all the tags
+
+	return " ".join(tags)
+
+def urlize(tags):
+	# Returns urls as list
+
+	urls = []
+	for tag in tags:
+		urls.append("https://favqs.com/api/quotes/?filter=" + tag)
+
+	return urls
+
+headers = {'Authorization' : 'Token token="b09904a4d885299ee386f5cbbf795c56"'}
+req = requests.get("https://favqs.com/api/quotes/?filter=mountain", headers = headers)
+
+def get_quotes(urls):
+    quotes = pd.DataFrame({'Quote': [], 'Author': []})
+    for url in urls:
+        req = requests.get(url, headers = headers).json()
+        time.sleep(0.5)
+        print "Processing: " + url
+        for item in req['quotes']:
+            if item['body'] != 'No quotes found':
+                quotes = quotes.append(pd.DataFrame({'Quote': [item['body']], 'Author': [item['author']]}))
+    return quotes.reset_index(drop=True)
+
+def similarity_matrix(dataframe):
+    tfidf = TfidfVectorizer(analyzer = 'word', ngram_range = (1,3), min_df = 0, stop_words = 'english')
+    tfidf_matrix = tfidf.fit_transform(dataframe)
+    cosine_similarities = linear_kernel(tfidf_matrix, tfidf_matrix)
+    return pd.DataFrame(cosine_similarities)
+
+def best_similarities(dataframe):
+    return dataframe[len(dataframe)-1].sort_values(ascending=False)[1:7].reset_index()
+
+def candidate_indexes(dataframe):
+	return dataframe['index']
+
+def candidate_captions(dataframe, indexes):
+	captions = []
+	for index in indexes:
+		captions.append(dataframe[index])
+	return captions
+
+
+def captionize(img):
+
+	tags = clarifaize(img)
+	tagPhrase = tagPhrase(tags)
+	urls = urlize(tags)
+	quotes = quotes = pd.DataFrame({'Quote': get_quotes(urls)['Quote'], 'Author': get_quotes(urls)['Author']})
+	phrases = quotes.append(pd.DataFrame({'Quote' : [tagPhrase], 'Author': ['No One']})).reset_index(drop=True)
+	matrix = similarity_matrix(phrases['Quote'])
+	best = best_similarities(matrix)
+	captions =candidate_captions(phrases['Quote'] + ' - ' + phrases['Author'], candidate_indexes(best_similarities(matrix)))
+
+	return captions
+
+def hashtagger(img):
+	tags = clarifaize(img)
+	alltags = []
+	for tag in tags[:7]:
+		alltags.append("#"+tag)
+	return alltags
